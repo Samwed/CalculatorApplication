920d34232f1a0b8f8da27b31f04b74dcfb445e6e
SammedBhistannavar <SammedBhistannavar@DESKTOP-66SRTHG>
2024-12-02 12:41:45 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1 = 0;
+int num2 = 0;
+Console.WriteLine("Enter num1 Value");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter num2 Value");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose the Calculation Mode");
+Console.WriteLine("\t a - Addtion");
+Console.WriteLine("\t b - Substraction");
+Console.WriteLine("\t c - Multipication");
+Console.WriteLine("\t d - Division");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine("num1 and num2 Addition is ->" + (num1 + num2));
+        break;
+
+    case "b":
+        Console.WriteLine("num1 and num2 Substaction is ->" + (num1 - num2));
+        break;
+
+    case "c":
+        Console.WriteLine("num1 and num2 Multipication is ->" + (num1 * num2));
+        break;
+
+    case "d":
+        Console.WriteLine("num1 and num2 Division is ->" + (num1 / num2));
+        break;
+
+}
+        
